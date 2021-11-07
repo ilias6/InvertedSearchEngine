571a5543c9e7f78886677a0c7ed75d437ee6b69f
ilias6 <ilias6@gmail.com>
2021-11-07 17:50:30 +0200

    listErrorCode #1

---
+++ include/list.hpp
@@ -0,0 +1,329 @@
+#ifndef __LIST_HPP__
+#define __LIST_HPP__
+
+#include <stdexcept>
+
+enum listErrorCode {SUCCESS, FAIL, EMPTY, NOT_EXISTS}
+
+using namespace std;
+
+template <typename T>
+class ListNode {
+    private:
+	T data;
+	ListNode<T> * next;
+    public:
+	ListNode<T>(T &);
+	~ListNode<T>();
+	void print() const;
+	void printAddr() const;
+	ListNode<T> * getNext() const;
+	T & getData();
+    	T getDataCopy();
+	void setNext(ListNode<T> *);
+	void setData(T&);
+};
+
+template <typename T>
+class List {
+    private:
+	int len;
+	ListNode<T> * head;
+	ListNode<T> * tail;
+    public:
+	List();
+	List(const List &);
+	~List();
+	void print() const;
+	void printAddr() const;
+	int getLen() const;
+    	void copyList(List &);
+	listErrorCode insert(T&, T ** t = NULL);
+	listErrorCode append(List<T> *);
+	bool exists(T&) const;
+	listErrorCode remove(T&);
+	T & getItem(int) const;
+	T * getItemPtr(int) const;
+    	T getItemCopy(int);
+
+};
+
+template <typename T>
+List<T>::List() {
+    this->head = NULL;
+    this->tail = NULL;
+    this->len = 0;
+}
+
+template <typename T>
+List<T>::List(const List & lst) {
+    this->len = lst.len;
+
+    if (lst.head == NULL) {
+        this->head = NULL;
+	    this->tail = NULL;
+        return;
+    }
+
+    // create new root:
+    this->head = new ListNode<T>(lst.head->getData());
+
+    ListNode<T> * lst_currentNode = lst.head;
+    ListNode<T> * this_currentNode = this->head;
+    while (lst_currentNode->getNext() != NULL) {
+        // create new successor:
+        ListNode<T> * newNode = new ListNode<T>(lst_currentNode->getNext()->getData());
+        this_currentNode->setNext(newNode);
+        this_currentNode = this_currentNode->getNext();
+        lst_currentNode = lst_currentNode->getNext();
+    }
+    this->tail = this_currentNode;
+    //cout << "List Copied!\n";
+}
+
+template <typename T>
+void List<T>::copyList(List & lst) {
+    if(this->len!=0){
+        ListNode<T> * lst_currentNode = this->head;
+        while(lst_currentNode!=NULL){
+            ListNode<T> * tmp=lst_currentNode->getNext();
+            delete lst_currentNode;
+            lst_currentNode=tmp;
+        }
+    }
+
+    this->len = lst.len;
+
+    if (lst.head == NULL) {
+        this->head = NULL;
+	this->tail = NULL;
+        return;
+    }
+
+    // create new root:
+    this->head = new ListNode<T>(lst.head->getData());
+
+    ListNode<T> * lst_currentNode = lst.head;
+    ListNode<T> * this_currentNode = this->head;
+    while (lst_currentNode->getNext() != NULL) {
+        // create new successor:
+        ListNode<T> * newNode = new ListNode<T>(lst_currentNode->getNext()->getData());
+        this_currentNode->setNext(newNode);
+        this_currentNode = this_currentNode->getNext();
+        lst_currentNode = lst_currentNode->getNext();
+    }
+    this->tail = this_currentNode;
+    //cout << "List Copied!\n";
+}
+
+template <typename T>
+List<T>::~List() {
+    ListNode<T> * lst = this->head;
+    while (lst != NULL) {
+	//cout << "Deleting: ";
+	//cout << lst->getData() << endl;
+	ListNode<T> * t = lst;
+	lst = lst->getNext();
+	delete t;
+    }
+}
+
+template <typename T>
+void List<T>::print() const {
+    ListNode<T> * lst = this->head;
+    while (lst != NULL) {
+	lst->print();
+	lst = lst->getNext();
+    }
+}
+
+template <typename T>
+void List<T>::printAddr() const {
+    ListNode<T> * lst = this->head;
+    while (lst != NULL) {
+	lst->printAddr();
+	lst = lst->getNext();
+    }
+}
+
+template <typename T>
+int List<T>::getLen() const {
+    return this->len;
+}
+
+template <typename T>
+T & List<T>::getItem(int index) const {
+    if (index >= this->len && index >= 0)
+	throw invalid_argument("Index out of range");
+
+    ListNode<T> * lst = this->head;
+    for (int i = 0; i < index; ++i)
+	lst = lst->getNext();
+
+    return lst->getData();
+
+}
+
+template <typename T>
+T * List<T>::getItemPtr(int index) const {
+    if (index >= this->len && index >= 0)
+	return NULL;
+    
+    ListNode<T> * lst = this->head;
+    for (int i = 0; i < index; ++i)
+	lst = lst->getNext();
+
+    return &lst->getData();
+
+}
+
+template <typename T>
+T List<T>::getItemCopy(int index) {
+    if (index >= this->len && index >= 0)
+	throw invalid_argument("Index out of range");
+
+    ListNode<T> * lst = this->head;
+    for (int i = 0; i < index; ++i)
+	lst = lst->getNext();
+
+    return lst->getDataCopy();
+
+}
+
+template <typename T>
+listErrorCode List<T>::append(List<T> * lst) {
+    /*The dump way*/
+    int len = lst->getLen();
+    for (int i = 0; i < len; ++i) {
+        T & item = lst->getItem(i);
+	    if (this->insert(item) == FAIL)
+		return FAIL;
+    }
+    return SUCCESS;
+    /*
+    if (lst.head == NULL)
+	return;
+
+    this->tail->setNext(lst.head);
+    this->tail = lst.tail;
+    */
+}
+
+template <typename T>
+listErrorCode List<T>::insert(T & item, T ** itemPtr) {
+    ListNode<T> * node = NULL;
+    try {
+	node = new ListNode<T>(item);
+    } 
+    catch (bad_alloc & exc) {
+	return FAIL;
+    }
+
+    if(itemPtr != NULL)
+        *itemPtr = &node->getData();
+
+    if (this->head == NULL) {
+	this->head = node;
+	this->tail = node;
+	this->len = 1;
+	return SUCCESS;
+    }
+
+    this->tail->setNext(node);
+    this->tail = node;
+    this->len++;
+}
+
+template <typename T>
+listErrorCode List<T>::remove(T& item) {
+    if (this->head == NULL) {
+	return EMPTY;
+    }
+
+    if (this->head->getData() == item) {
+	ListNode<T> * t = this->head;
+	this->head = this->head->getNext();
+	delete t;
+	this->len--;
+	if (this->len == 0)
+	    this->tail = NULL;
+	return SUCCESS;
+    }
+
+    ListNode<T> * n1 = this->head;
+    ListNode<T> * n2 = this->head->getNext();
+    while (n2 != NULL) {
+	if (n2->getData() == item) {
+	    n1->setNext(n2->getNext());
+	    if (n2 == this->tail)
+		this->tail = n1;
+	    delete n2;
+	    this->len--;
+	    return SUCCESS;
+	}
+	n1 = n2;
+	n2 = n2->getNext();
+    }
+    return NOT_EXISTS
+
+}
+
+template <typename T>
+bool List<T>::exists(T& item) const {
+    ListNode<T> * lst = this->head;
+    while (lst != NULL) {
+	if (lst->getData() == item)
+	    return true;
+	lst = lst->getNext();
+    }
+    return false;
+
+}
+
+template <typename T>
+ListNode<T>::ListNode(T& data):data(data){
+    // this->data=data;
+    this->next = NULL;
+}
+
+template <typename T>
+ListNode<T>::~ListNode() {}
+
+template <typename T>
+void ListNode<T>::setNext(ListNode<T> * newNode) {
+    this->next = newNode;
+}
+
+
+template <typename T>
+void ListNode<T>::setData(T& item){
+    this->data.set(item);
+}
+
+template <typename T>
+void ListNode<T>::print() const {
+    std::cout << this->data << std::endl;
+}
+
+template <typename T>
+void ListNode<T>::printAddr() const {
+    std::cout << &this->data << std::endl;
+}
+
+template <typename T>
+ListNode<T> * ListNode<T>::getNext() const {
+    return this->next;
+}
+
+template <typename T>
+T & ListNode<T>::getData() {
+    return this->data;
+}
+
+template <typename T>
+T ListNode<T>::getDataCopy() {
+    return this->data;
+}
+
+
+#endif
