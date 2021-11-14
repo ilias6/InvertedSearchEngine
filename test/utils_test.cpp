cd0ac35f5905efeb95032ee46e2afcb836145a3b
ilias6 <ilias6k@gmail.com>
2021-11-14 20:01:59 +0200

    utils test #1

---
+++ test/utils_test.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "../include/utils.hpp"
+
+
+TEST(isPrimeTest, NotPrime) {
+    int nums[] = {0, -2, 10, -80, 54280};
+    for (int i = 0; i < 5; ++i)
+	ASSERT_TRUE(false == isPrime(nums[i]));
+}
+
+TEST(isPrimeTest, Prime) {
+    int nums[] = {0, 3, 7, 83, 7919};
+    for (int i = 0; i < 5; ++i)
+	ASSERT_TRUE(true == isPrime(nums[i]));
+}
+
+TEST(findNextPrimeTest, Simple) {
+    int n = 54280;
+    int p = findNextPrime(n);
+    for (int i = n; i < p; ++i)
+	ASSERT_TRUE(false == isPrime(i));
+}
+
+TEST(findNextPrimeTest, SameNum) {
+    int n = 7919;
+    int p = findNextPrime(n);
+    ASSERT_TRUE(n == p);
+}
