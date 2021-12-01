a941e5a6d079a489615c2fffed79751e81672b04
1n5ane <th.lolos1000@gmail.com>
2021-12-01 19:19:09 +0200

    DocumentTestToBeDone

---
+++ test/document_test.cpp
@@ -0,0 +1,25 @@
+#include "../include/document.hpp"
+#include "../include/gtest.h"
+#include <stdexcept>
+#include <iostream>
+using namespace std;
+
+class DocumentTest: public ::testing::Test {
+    protected:
+        Document ** doc_arr;
+        int size;
+        virtual void SetUp() {
+
+        }
+
+        virtual void TearDown() {
+            for(int i=0;i<size;i++)
+                delete doc_arr[i];
+            delete[] doc_arr;
+        }
+};
+
+TEST_F(DocumentTest,ConstructorTest){
+    this->size=3;//3 docs
+    const char * arr[]={"2 6243 http dbpedia resource list people from minnesota http"};
+}
